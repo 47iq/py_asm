bf9b9fcc736d7dd00d5369c511eac7f6fc7365ab
Pavel Danilov <pavel.danilov@tune-it.ru>
2023-01-20 04:14:09 +0300

    init

---
+++ examples/var_test.asm
@@ -0,0 +1,17 @@
+section data
+    word 'h'
+    word 'e'
+    word 'l'
+    word 'l'
+    word 'o'
+    int 0 1
+    word ' '
+jopa:
+    word 'w'
+    word 'o'
+    word 'r'
+    word 'l'
+    word 'd'
+section text
+    ld r1 jopa
+    out r1
